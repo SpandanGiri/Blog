1f519230d1e0cf51e3e1d199000d4d1af1b23294
Spandan <garov57775@gmail.com>
2021-07-01 00:24:28 +0530

    done with profile

--- blog/urls.py
+++ blog/urls.py
@@ -17,15 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',views.blog,name="blog"),
     path('about/',views.about,name="about"),
     path('user/', include('users.urls')),
-    
-    
-   
-    
-]
+     
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
