dda02b924140a54f9ec674c64b7cf4df063b3584
Spandan <garov57775@gmail.com>
2021-06-30 11:46:41 +0530

    updating users

--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 
 def register(request):
     form=UserCreationForm()
-    return render(request, 'users/register.html',{'form':form})
+    
 
+    if(request.method=='POST'):
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            print(username)
+            messages.success(request, f'Account created for {username}')
+            return redirect('http://127.0.0.1:8000/blog/')
+            
+    else:
+        form=UserCreationForm()
+        
+    return render(request, 'users/register.html',{'form':form})
